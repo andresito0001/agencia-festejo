2143395e358893f8e289fe34edae53455fc78688
andresito0001 <70287752+andresito0001@users.noreply.github.com>
2024-12-03 16:27:57 -0400

    Create Table.java

---
+++ src/utility/Table.java
@@ -0,0 +1,26 @@
+package utility;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Table {
+    public static void createTable(final Connection conn, final String name, final List<String> colums,
+                                    final List<String> types) throws SQLException {
+        StringBuilder sql = new StringBuilder("create table " + name + " (");
+
+        for (int i = 0; i < colums.size(); i++) {
+            sql.append(colums.get(i)).append(" ").append(types.get(i));
+                if (i < colums.size() - 1) {
+                sql.append(", ");
+            }
+        }
+        sql.append(");");
+
+        try (final PreparedStatement st = conn.prepareStatement(sql.toString())) {
+            st.executeUpdate();
+            st.close();
+        }
+    }
+}
